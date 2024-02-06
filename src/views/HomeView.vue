692b87e307b4e47918ef058fc43bb83d788040a4
tillymee <tilly@Tillys-Air.fritz.box>
2024-02-06 23:13:53 +0100

    button color jump

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+
 function toggle() {
   state.value = !state.value
 }
 
 const state = ref(false)
+const isHovered = ref(false)
 </script>
 
 <template>
   <main>SUPERMOM</main>
-  <button class="clicker" @click="toggle">Click me</button>
+  <button
+    class="clicker"
+    @click="toggle"
+    @mouseover="isHovered = true"
+    @mouseleave="isHovered = false"
+    :class="{ jump: isHovered }"
+  >
+    Click me
+  </button>
 
   <div v-if="state" class="time">18 Uhr</div>
 </template>
@@ -19,6 +29,15 @@ const state = ref(false)
   margin: 20px 0 0 50px;
   background-color: aqua;
   border-radius: 20px;
+  transition: transform 0.3s ease;
+}
+
+.clicker:hover {
+  background-color: bisque;
+}
+
+.clicker.jump {
+  transform: translateY(-5px);
 }
 
 .time {
